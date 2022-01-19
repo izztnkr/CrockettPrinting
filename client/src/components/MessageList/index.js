cefd2aee6a6e6bbc055dba0d65bd1bd071d0f671
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2022-01-19 15:16:15 -0600

    import of Mutations done, troubleshooting in progress

--- client/src/components/MessageList/index.js
+++ client/src/components/MessageList/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../MessageList/style.css";
+import { useMutation } from "@apollo/client";
+import { REMOVE_MESSAGE } from "../../utils/mutations";
+import { useParams } from "react-router-dom";
 
 //messageList component for app. This will show a list of all the messages which exist and will also prompt users to respond if necessary
 const MessageList = ({ messages, title }) => {
+  const params = useParams();
+  console.log(params);
+  const { messageId } = useParams();
+  const [removeMessage] = useMutation(REMOVE_MESSAGE, {
+    variables: { messageId: params.id }
+  });
+  
+  // const { loading, data } = useMutation(REMOVE_MESSAGE, {
+  //   variables: { categoryId: params.id },
+  // });
+  // const category = data?.category || [];
+  // console.log(category);
+  // const deleteMessage = () => {
+  //   removeMessage({
+  //     variables: { messageId: messageId },
+  //   });
+  // };
   if (!messages.length) {
     return <h3>No Messages Yet</h3>;
   }
@@ -22,6 +42,7 @@ const MessageList = ({ messages, title }) => {
             </h4>
             <div className="card-body bg-light p-2">
               <p>{message.messageText}</p>
+              <button messageId={message._id} onClick={removeMessage}>Delete Message</button>
             </div>
             <Link
               className="btn btn-primary btn-block btn-squared"
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,7 +15,7 @@ const resolvers = {
       return Message.find(params).sort({ createdAt: -1 });
     },
     message: async (parent, { messageId }) => {
-      return Message.findOne({ _id: messageId });
+      return Message.findOne({ id: messageId });
     },
     messages: async (parent, { username }) => {
       return Message.find().sort({ createdAt: -1 });
@@ -25,7 +25,7 @@ const resolvers = {
       return Category.find();
     },
     category: async (parent, { categoryId }) => {
-      return Category.findOne({ _id:categoryId });
+      return Category.findOne({ _id: categoryId });
     },
   },
 
@@ -82,8 +82,8 @@ const resolvers = {
     },
     removeResponse: async (parent, { messageId, responseId }) => {
       return Message.findOneAndUpdate(
-        { _id: messageId },
-        { $pull: { responses: { _id: responseId } } },
+        { id: messageId },
+        { $pull: { responses: { id: responseId } } },
         { new: true }
       );
     },
